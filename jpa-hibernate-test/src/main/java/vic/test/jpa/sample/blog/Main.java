ade1d888934ba823fe115b2010ead3dd8995de3f
Vic Liu <vic.liu@eroad.com>
2016-01-21 10:32:31 +1300

    jpa-hibernate-test: 1. register all entity classes to persistence.xml, since in non-jee environment, no autoscan 2. use logback instead of log4j

--- jpa-hibernate-test/src/main/java/vic/test/jpa/sample/blog/Main.java
+++ jpa-hibernate-test/src/main/java/vic/test/jpa/sample/blog/Main.java
@@ -3,9 +3,11 @@ package vic.test.jpa.sample.blog;
 import java.util.Date;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import vic.test.jpa.PersistenceManager;
 
@@ -14,11 +16,13 @@ public class Main {
 	private final static Logger LOG = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		
+
+
 		// app init
 		EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
-		
+
 		Date now = new Date();
+		MDC.put("requestId", String.valueOf(System.currentTimeMillis()));
 		// init categories
 		em.clear();
 		em.getTransaction().begin();
@@ -34,8 +38,9 @@ public class Main {
 		LOG.info("Created categories");
 		LOG.info("  " + cateJava);
 		LOG.info("  " + cateSpring);
-		
+
 		// init user
+		MDC.put("requestId", String.valueOf(System.currentTimeMillis()));
 		em.clear();
 		em.getTransaction().begin();
 		BlogUser vic = new BlogUser();
@@ -45,9 +50,10 @@ public class Main {
 		em.getTransaction().commit();
 		LOG.info("Created users");
 		LOG.info("  " + vic);
-		
-		
+
+
 		// Posting Blog
+		MDC.put("requestId", String.valueOf(System.currentTimeMillis()));
 		Blog blog;
 		blog = new Blog();
 		blog.setTitle("blog1");
@@ -56,6 +62,7 @@ public class Main {
 		blog = post(em, cateJava.getId(), blog);
 		LOG.info("Created blog " + blog);
 
+		MDC.put("requestId", String.valueOf(System.currentTimeMillis()));
 		blog = new Blog();
 		blog.setTitle("blog2");
 		blog.setAuthor(vic);
@@ -63,21 +70,24 @@ public class Main {
 		blog = post(em, cateJava.getId(), blog);
 		LOG.info("Created blog " + blog);
 
+		MDC.put("requestId", String.valueOf(System.currentTimeMillis()));
 		blog = new Blog();
 		blog.setTitle("blog3");
 		blog.setAuthor(vic);
 		blog.setCreatedAt(now);
 		blog = post(em, cateJava.getId(), blog);
 		LOG.info("Created blog " + blog);
-		
-		
+
+
 		// list
+		MDC.put("requestId", String.valueOf(System.currentTimeMillis()));
 		listBlogs(em, vic.getId());
-		
+
+		MDC.put("requestId", String.valueOf(System.currentTimeMillis()));
 		em.close();
 		PersistenceManager.INSTANCE.close();
 	}
-	
+
 	private static void listBlogs(EntityManager em, Long userId) {
 		em.clear();
 		LOG.info(">> Listing blogs of user " + userId);
@@ -86,7 +96,7 @@ public class Main {
 			LOG.info(blog.toString());
 		}
 	}
-	
+
 	private static Blog post(EntityManager em, Long cateId, Blog newBlog) {
 		em.getTransaction().begin();
 		Category category = em.find(Category.class, cateId);
@@ -96,7 +106,7 @@ public class Main {
 		em.getTransaction().commit();
 		return newBlog;
 	}
-	
-	
-	
+
+
+
 }
